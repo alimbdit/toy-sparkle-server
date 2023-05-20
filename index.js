9d499a0836e5cf3cc2383af91168c67839da57c0
alimbdit <alimbdit@gmail.com>
2023-05-20 16:32:16 +0600

    toys by user email added

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/toys/:email', async(req, res) => {
+      const email = req.params.email;
+      const result = await toyCollection.find({sellerEmail: email}).toArray();
+      res.send(result)
+    })
+
     app.get('/toy/:id', async(req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };

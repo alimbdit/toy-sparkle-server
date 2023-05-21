24a539cf03301466bc6435780f2fa68082e47249
alimbdit <alimbdit@gmail.com>
2023-05-21 23:26:06 +0600

    okey

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
-
-
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
+const blogs = require('./data/blog.json');
+const reviews = require('./data/reviews.json');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h73vuqp.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,87 +23,116 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-
-    
     const toyCollection = client.db("toySparkleDB").collection("allToys");
 
-
-    app.post('/toys', async(req, res) => {
-        const body = req.body;
-        console.log(body)
-        const result = await toyCollection.insertOne(body);
-        res.send(result)
-    })
-
-    app.get('/toys', async(req, res) => {
-        let query = {}
-
-        const options = {
-        
-        projection: { toyName: 1, price: 1, sellerName: 1, subCategory: 1, quantity: 1 },
-      };
-        const result = await toyCollection.find(query, options).limit(20).toArray();
-        res.send(result)
-    })
-
-    app.get('/toysByName/:name', async(req, res) => {
-
-      const name = req.params.name;
-      
-      const result = await toyCollection.find({ toyName: { $regex: name, $options: 'i' } }).toArray();
+    app.post("/toys", async (req, res) => {
+      const body = req.body;
+      console.log(body);
+      const result = await toyCollection.insertOne(body);
       res.send(result);
-    })
+    });
 
+    app.get("/toys", async (req, res) => {
+      let query = {};
 
+      const options = {
+        projection: {
+          toyName: 1,
+          price: 1,
+          sellerName: 1,
+          subCategory: 1,
+          quantity: 1,
+        },
+      };
+      const result = await toyCollection
+        .find(query, options)
+        .limit(20)
+        .toArray();
+      res.send(result);
+    });
 
-    app.post('/toys/:email', async(req, res) => {
+    app.get("/toysByName/:name", async (req, res) => {
+      const name = req.params.name;
+
+      const result = await toyCollection
+        .find({ toyName: { $regex: name, $options: "i" } })
+        .toArray();
+      res.send(result);
+    });
+
+    app.patch("/updateToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const filter = { _id: new ObjectId(id) };
+      // console.log(body)
+      const updateDoc = {
+        $set: {
+          price: body.price,
+          quantity: body.quantity,
+          description: body.description,
+        },
+      };
+      const result = await toyCollection.updateOne(filter, updateDoc);
+      res.send(result)
+    });
+
+    app.post("/toys/:email", async (req, res) => {
       const email = req.params.email;
-      console.log("72",req.body)
+      // console.log("72", req.body);
       let sort;
-      if(req.body.sortText === 'ascending'){
-        sort = 1
+      if (req.body.sortText === "ascending") {
+        sort = 1;
+      } else if (req.body.sortText === "descending") {
+        sort = -1;
       }
-      else if(req.body.sortText === 'descending'){
-        sort = -1
-      }
-      const result = await toyCollection.find({sellerEmail: email}).sort({price: sort}).toArray();
-      res.send(result)
-    })
+      const result = await toyCollection
+        .find({ sellerEmail: email })
+        .sort({ price: sort })
+        .toArray();
+      res.send(result);
+    });
 
-   
-
-    app.get('/toys/:email', async(req, res) => {
+    app.get("/toys/:email", async (req, res) => {
       const email = req.params.email;
-    //  console.log(req.body)
-      const result = await toyCollection.find({sellerEmail: email}).toArray();
-      res.send(result)
-    })
+      //  console.log(req.body)
+      const result = await toyCollection.find({ sellerEmail: email }).toArray();
+      res.send(result);
+    });
+    app.get("/toyBySubCategory/:subCategory", async (req, res) => {
+      const subCategory = req.params.subCategory;
+      // console.log(subCategory)
+      //  console.log(req.body)
+      const result = await toyCollection.find({ subCategory: subCategory }).toArray();
+      res.send(result);
+    });
 
-    app.delete('/toy/:id', async(req, res) => {
+    app.delete("/toy/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toyCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/toy/:id', async(req, res) => {
+    app.get("/toy/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await toyCollection.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -114,12 +140,18 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("toySparkle is running");
+});
 
-app.get('/', (req, res) => {
-    res.send('toySparkle is running')
+app.get('/blogs', (req, res) => {
+  res.send(blogs)
 })
 
-
-app.listen(port, ()=> {
-    console.log(`toySparkle is running on port ${port}`)
+app.get('/reviews', (req, res) => {
+  res.send(reviews)
 })
+
+app.listen(port, () => {
+  console.log(`toySparkle is running on port ${port}`);
+});
